ba964d65c658b2b0eceb32e900572b4eefc0339a
bavis <sai.bavirisetty@in.pega.com>
2018-11-27 17:21:10 +0530

    dgf

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    state = {}
+    render() {
+        return (
+
+            <div className="main">
+                <p className="sign" align="center">Todos</p>
+                <input className="un" type="text" placeholder="Todo" />
+                <button className="submit" align="">Add</button>
+                <div>
+                    <input className=" sign" type="checkbox" id="test1" />
+                    <label for="test1">Red</label>
+                </div>
+
+            </div>
+        );
+    }
 }
 
 export default App;
